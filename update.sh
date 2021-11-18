e053785dfc7e01ea2910eb139b656e40f23d8b60
Extacy <git@extacy.cc>
2021-11-18 13:43:01 +0000

    Add auto update script

---
+++ update.sh
@@ -0,0 +1,19 @@
+arr=("api" "website")
+
+# Take down containers and clear images
+for i in "${arr[@]}"; do
+    cd $i
+    docker-compose down
+    docker rmi "realmsense/$i"
+    cd ..
+done
+
+# Update submodules
+git submodule update --remote --recursive
+
+# Restart all containers & build images
+for i in "${arr[@]}"; do
+    cd $i
+    ./start.sh
+    cd ..
+done
